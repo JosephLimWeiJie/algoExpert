b6dd6ebc23ba3892ee84507216bf24af9488fb65
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2020-10-10 02:10:25 +0800

    Fix indent issues.

--- InsertionSort.java
+++ InsertionSort.java
@@ -6,24 +6,24 @@ class Program {
         // Time: O(n ** 2) Worst-case
         // Space: O(1)
 
-		if (array.length == 0) {
-			return new int[] {};
-		}
+        if (array.length == 0) {
+            return new int[] {};
+        }
 
-		for (int i = 1; i < array.length; i++) {
-			int j = i;
-			while (j > 0 && array[j] < array[j - 1]) {
-				swap(j, j - 1, array);
-				j--;
-			}
-		}
+        for (int i = 1; i < array.length; i++) {
+            int j = i;
+            while (j > 0 && array[j] < array[j - 1]) {
+                swap(j, j - 1, array);
+                j--;
+            }
+        }
 
         return array;
     }
 
-	public static void swap(int first, int second, int[] array) {
-		int temp = array[first];
-		array[first] = array[second];
-		array[second] = temp;
-	}
+    public static void swap(int first, int second, int[] array) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
 }
