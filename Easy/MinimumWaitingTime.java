09e3c0042d7e3d1a5ef77cb14f0d1fdb081f716d
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2021-01-22 17:06:47 +0800

    Add solution.

---
+++ Easy/MinimumWaitingTime.java
@@ -0,0 +1,29 @@
+package Easy;
+
+import java.util.*;
+
+class Program {
+
+    public static int minimumWaitingTime(int[] queries) {
+
+        // Time:  O()
+        // Space: O()
+
+        Arrays.sort(queries);
+        int waitingTime = 0;
+        int prev = 0;
+
+        for (int i = 0; i < queries.length - 1; i++) {
+            waitingTime += (queries[i] + prev);
+            prev += (queries[i]);
+        }
+
+        return waitingTime;
+    }
+
+    public static void main(String[] args) {
+        int[] queries = new int[] {3, 2, 1, 2, 6};
+        System.out.println(minimumWaitingTime(queries));
+    }
+}
+
