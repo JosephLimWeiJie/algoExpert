8e5c89a294af93a09ba33a899be518c156ce0e74
Joseph Lim <59989652+JosephLimWeiJie@users.noreply.github.com>
2021-04-23 11:32:54 +0800

    Update solutions.

---
+++ prompt/misc/KosarajuAlgorithm.java
@@ -0,0 +1,23 @@
+package prompt.misc;
+
+public class KosarajuAlgorithm {
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Input file has the following format:
+     * 
+     * First line represents the total number of nodes.
+     * For each subsequent line, the first integer represents the number of edges for node i,
+     * followed by the neighbor node and edge weight pair.
+     * 
+     * For example, line 2 means that node with ID 0 has 1 edge pair, which node 0 is linked
+     * to node 1 with a weight of 7.
+     */ 
+}
