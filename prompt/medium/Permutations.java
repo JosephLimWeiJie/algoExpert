009fc3ff999ca32ceee05be928e66a336b061554
Joseph Lim <limweijie@live.com.sg>
2021-06-24 11:23:42 +0800

    Add solutions.

---
+++ prompt/medium/Permutations.java
@@ -0,0 +1,36 @@
+package prompt.medium;
+
+import java.util.*;
+
+public class Permutations {
+
+    public static List<List<Integer>> getPermutations(List<Integer> array) {
+        // Write your code here.
+        return new ArrayList<List<Integer>>();
+    }
+    
+    public static void TestCase1() {
+        List<Integer> input = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+        List<List<Integer>> perms = getPermutations(input);
+        System.out.println(perms.size() == 6);
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(1, 2, 3))));
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(1, 3, 2))));
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(2, 1, 3))));
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(2, 3, 1))));
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(3, 1, 2))));
+        System.out.println(contains(perms, new ArrayList<Integer>(Arrays.asList(3, 2, 1))));
+    }
+    
+    public static boolean contains(List<List<Integer>> arr1, List<Integer> arr2) {
+        for (List<Integer> subArray : arr1) {
+            if (subArray.equals(arr2)) {
+            return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        TestCase1();
+    }
+}
